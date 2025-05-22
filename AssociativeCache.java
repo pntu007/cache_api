f0a5d0600f39113fb278c48ad714478b8785938a
sauravatgithub-web <22cs01010@iitbbs.ac.in>
2025-05-22 11:24:49 +0530

    Added central Cache class and DMCache and Associative Cache

---
+++ AssociativeCache.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import cache.Cache;
+
+public class AssociativeCache extends Cache {
+    static int ways = 2;
+
+    public AssociativeCache() {
+        super(1, ways);
+    }
+
+    public static void main(String[] args) {
+        AssociativeCache cache = new AssociativeCache();
+        // Block[][] cacheBlocks = cache.getCache();
+        List<Long> manualAddresses = Arrays.asList(
+            64L, 128L, 64L, 256L, 64L, 128L, 512L, 1024L, 64L
+        );
+        cache.handleManualRequests(manualAddresses);
+    }
+}
---
+++ DirectMappedCache.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import cache.Cache;
+
+public class DirectMappedCache extends Cache {
+    static int sets = 2;
+
+    public DirectMappedCache() {
+        super(sets, 1);
+    }
+
+    public static void main(String[] args) {
+        DirectMappedCache cache = new DirectMappedCache();
+        // Block[][] cacheBlocks = cache.getCache();
+        List<Long> manualAddresses = Arrays.asList(
+            64L, 128L, 64L, 256L, 64L, 128L, 512L, 1024L, 64L
+        );
+        cache.handleManualRequests(manualAddresses);
+    }
+}
