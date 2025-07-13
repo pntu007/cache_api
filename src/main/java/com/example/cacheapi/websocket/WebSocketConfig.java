ab086b072eb8f0d80d27503a2e671515e7ff89b6
Harsh Maurya <dardementor@gmail.com>
2025-07-13 11:44:34 +0530

    updated frontend url

--- src/main/java/com/example/cacheapi/websocket/WebSocketConfig.java
+++ src/main/java/com/example/cacheapi/websocket/WebSocketConfig.java
@@ -11,7 +11,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(cacheWebSocketHandler(), "/ws/cache")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("https://cache-visualizer.vercel.app");
     }
 
     @Bean
